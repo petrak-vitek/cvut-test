5367e73279246b5c58b14e6d7c1d8190f10b48be
Vít Petrák <you@example.com>
2024-05-23 22:00:42 +0200

    vytvoreni testovaciho kodu

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// Funkce pro sečtení dvou čísel
+int scitani(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int cislo1, cislo2, vysledek;
+
+    // Načtení dvou čísel od uživatele
+    std::cout << "Zadejte prvni cislo: ";
+    std::cin >> cislo1;
+    std::cout << "Zadejte druhe cislo: ";
+    std::cin >> cislo2;
+
+    // Volání funkce pro sečtení čísel
+    vysledek = scitani(cislo1, cislo2);
+
+    // Výpis výsledku na obrazovku
+    std::cout << "Součet je: " << vysledek << std::endl;
+
+    return 0;
+}
